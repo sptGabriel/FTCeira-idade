130d7d017399748e5f3d8274b66a5e45ae23216e
Gabriel <gabrielcerqueira96@hotmail.com>
2021-04-02 00:24:39 -0300

    refactor(middleware): change middlewares file names

---
+++ packages/backend/src/infrastructure/express/middlewares/metrics-middleware.ts
@@ -0,0 +1,9 @@
+const promBundle = require('express-prom-bundle')
+
+export const metricsMiddleware = promBundle({
+  includeMethod: true,
+  includePath: true,
+  customLabels: {
+    project_version: '3.0',
+  },
+})

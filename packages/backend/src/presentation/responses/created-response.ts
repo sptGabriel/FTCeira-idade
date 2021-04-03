05b19db8d40409d517ca04f8d719e52787f53cc0
Gabriel <gabrielcerqueira96@hotmail.com>
2021-04-02 23:00:55 -0300

    refactor(presentation): responses

--- packages/backend/src/presentation/responses/created-response.ts
+++ packages/backend/src/presentation/responses/created-response.ts
@@ -1,5 +1,5 @@
-import { IResponseHandler } from '~/application/ports/response-handler';
-import { IResponseModel } from '~/application/ports/response-model';
+import { IResponseHandler } from '~/application/ports/response-handler'
+import { IResponseModel } from '~/application/ports/response-model'
 
 export class DeletedResponse implements IResponseHandler<void> {
   async response(): Promise<IResponseModel<void>> {
--- packages/backend/src/presentation/responses/sucess-response.ts
+++ packages/backend/src/presentation/responses/sucess-response.ts
@@ -1,12 +1,11 @@
-import { IResponseHandler } from "~/application/ports/response-handler";
-import { IResponseModel } from "~/application/ports/response-model";
-
+import { IResponseHandler } from '~/application/ports/response-handler'
+import { IResponseModel } from '~/application/ports/response-model'
 
 export class SuccessResponse<T> implements IResponseHandler<T> {
   async response(body: T): Promise<IResponseModel<T>> {
     return {
       statusCode: 200,
       body,
-    };
+    }
   }
 }

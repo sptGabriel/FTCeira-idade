3029738fecd497aaa16f647212160cf8a17b3c20
Gabriel <gabrielcerqueira96@hotmail.com>
2021-04-20 01:43:36 -0300

    refactor: error handler

--- packages/backend/src/application/errors/unprocessable-entity.error.ts
+++ packages/backend/src/application/errors/unprocessable-entity.error.ts
@@ -1,34 +1,25 @@
 import { BaseError } from './base-app.error'
 
-export enum UnprocessableCodes {
-  'missing_field',
-  'missing',
-  'invalid',
-  'already_exists',
-  'unprocessable',
-}
-
 export interface IUnprocessableModel {
-  resource: string
   field: string
-  code: keyof typeof UnprocessableCodes
+  type: string
 }
 
-export class UnprocessableEntityERROR extends BaseError {
+export class UnprocessableERROR extends BaseError {
   private errors = [] as IUnprocessableModel[]
-  constructor(errors?: IUnprocessableModel[]) {
+  constructor(
+    private readonly resource: string,
+    errors?: IUnprocessableModel[],
+  ) {
     super('Validation Failed')
     this.statusCode = 422
     this.name = 'UNPROCESSABLE_ERROR'
-    if (errors) this.errors.push(...errors)
-  }
-
-  public addError(model: IUnprocessableModel) {
-    this.errors.push(model)
+    if (errors) this.errors = errors
   }
 
   public serialize() {
-    const { message, statusCode, errors } = this
-    return { message, statusCode, errors: errors.length > 0 || undefined }
+    const { message, statusCode } = this
+    const errors = this.errors.length > 0 ? this.errors : undefined
+    return { message, statusCode, errors }
   }
 }

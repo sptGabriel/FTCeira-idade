635763705ffcc1044a73a248325b1dbaa71cb8ba
Jobson de Almeida <jobson.ra@outlook.com>
2021-04-25 17:40:12 -0300

    build: home page tooltip

--- packages/frontend/src/views/home/HomeView/Component.js
+++ packages/frontend/src/views/home/HomeView/Component.js
@@ -5,15 +5,16 @@ import {
   Card,
   CardContent,
   Typography,
-  makeStyles
+  makeStyles,
+  Box,
+  Divider
 } from '@material-ui/core';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
-
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
-
-// import { useNavigate } from 'react-router-dom';
+import CustomTooltip from 'src/utils/CustomTooltip';
+import { useNavigate } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -29,45 +30,73 @@ const useStyles = makeStyles((theme) => ({
   },
   handPointer: {
     cursor: 'pointer'
+  },
+  details: {
+    minHeight: 240
+  },
+  media: {
+    padding: theme.spacing(1)
+  },
+  footer: {
+    backgroundColor: 'primary'
   }
 }));
 
 const Component = ({ className, course, ...rest }) => {
   const classes = useStyles();
-  // const navigate = useNavigate();
-  // const clickActions = () => {
-  //  navigate('', { replace: false });
-  // };
+  const navigate = useNavigate();
+
+  const clickActions = (action) => {
+    switch (action) {
+      case 'view':
+        navigate('/app/course-view', { replace: false });
+        break;
+      default:
+        break;
+    }
+  };
 
   return (
     <Card
       className={clsx(classes.root, className)}
       {...rest}
     >
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          alt="Contemplative Reptile"
-          height="240"
-          image={course.media}
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {course.title}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {course.description}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {course.category}
-          </Typography>
-        </CardContent>
+
+      <CardActionArea onClick={() => clickActions('view')}>
+        <CustomTooltip title="Clique para mais informações" placement="bottom-end">
+          <CardMedia
+            component="img"
+            alt=""
+            height="360"
+            image={course.media}
+            title=""
+            className={classes.media}
+          />
+        </CustomTooltip>
+        <CustomTooltip title="Clique para mais informações" placement="top-end">
+          <CardContent className={classes.details}>
+            <Typography gutterBottom variant="h3" component="h2">
+              {course.title}
+            </Typography>
+            <Typography variant="body1" color="textSecondary" component="p">
+              {course.description}
+            </Typography>
+          </CardContent>
+        </CustomTooltip>
       </CardActionArea>
+      <Divider />
       <CardActions>
-        <Button size="small" color="primary">
-          inscreva-se
-        </Button>
+        <Box className={classes.footer}>
+          <CustomTooltip title="Clique aqui e inscreva-se nesse curso">
+            <Button
+              color="primary"
+              size="large"
+              onClick={() => clickActions('view')}
+            >
+              inscreva-se
+            </Button>
+          </CustomTooltip>
+        </Box>
       </CardActions>
     </Card>
   );

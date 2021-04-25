016935d4a33860811e962494699eec75bc970cf5
Jobson de Almeida <jobson.ra@outlook.com>
2021-04-25 17:40:12 -0300

    build: home page tooltip

--- packages/frontend/src/views/home/HomeView/index.js
+++ packages/frontend/src/views/home/HomeView/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import {
-  Button,
   Container,
   makeStyles
 } from '@material-ui/core';
 import Page from 'src/components/Page';
-import { useNavigate } from 'react-router-dom';
 import ComponentCourse from './ComponentCourse';
 import data from './data';
 
@@ -27,32 +25,14 @@ const useStyles = makeStyles((theme) => ({
 
 const HomeView = () => {
   const classes = useStyles();
-  const navigate = useNavigate();
-
-  const clickActions = () => {
-    navigate('/app/student-assessments', { replace: false });
-  };
 
   return (
     <Page
       className={classes.root}
       title="Home"
     >
-      <Button color="primary" onClick={() => clickActions()}>propisório avaliações discente</Button>
-
       <Container maxWidth={false}>
-        <ComponentCourse courses={data} courseCategory="Sistemas de Informação" />
-        <ComponentCourse courses={data} courseCategory="Biomedicina" />
-        <ComponentCourse courses={data} courseCategory="Direito" />
-        <ComponentCourse courses={data} courseCategory="Enfermagem" />
-        <ComponentCourse courses={data} courseCategory="Engenharia Ambiental" />
-        <ComponentCourse courses={data} courseCategory="Engenharia Civil" />
-        <ComponentCourse courses={data} courseCategory="Farmácia" />
-        <ComponentCourse courses={data} courseCategory="Fisioterapia" />
-        <ComponentCourse courses={data} courseCategory="Medicina Veterinária" />
-        <ComponentCourse courses={data} courseCategory="Nutrição" />
-        <ComponentCourse courses={data} courseCategory="Odontologia" />
-        <ComponentCourse courses={data} courseCategory="Psicologia" />
+        <ComponentCourse courses={data} />
       </Container>
     </Page>
   );

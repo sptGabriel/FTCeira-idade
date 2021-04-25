857f62dd829b09d97608ad1447aeb854ce9c1242
Jobson de Almeida <jobson.ra@outlook.com>
2021-04-25 18:44:12 -0300

    build: classroom edit

---
+++ packages/frontend/src/views/classroom/ClassRoomEdit/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {
+  Container,
+  Grid,
+  makeStyles
+} from '@material-ui/core';
+import Page from 'src/components/Page';
+import ClassRoomEditDetails from './ClassRoomEditDetails';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.dark,
+    minHeight: '100%',
+    paddingBottom: theme.spacing(3),
+    paddingTop: theme.spacing(3)
+  }
+}));
+
+const ClassRoomEdit = () => {
+  const classes = useStyles();
+
+  return (
+    <Page
+      className={classes.root}
+      title="Turma"
+    >
+      <Container maxWidth="lg">
+        {/* <Container maxWidth="false"> */}
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          alignItems="flex-start"
+          spacing={3}
+        >
+          <Grid
+            item
+            lg={8}
+            md={6}
+            xs={12}
+          >
+            <ClassRoomEditDetails />
+          </Grid>
+        </Grid>
+      </Container>
+    </Page>
+  );
+};
+
+export default ClassRoomEdit;

7c0ac10882060c0fb87762abea2bdc400cb49c04
Jobson de Almeida <jobson.ra@outlook.com>
2021-05-21 19:26:41 -0300

    refactor: initial enrollment list code

--- packages/frontend/src/views/enrollment/EnrollmentListView/index.js
+++ packages/frontend/src/views/enrollment/EnrollmentListView/index.js
@@ -1,29 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Box,
+  Grid,
   Container,
   makeStyles
 } from '@material-ui/core';
 import Page from 'src/components/Page';
+import Component from './Component';
+import data from './data';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     backgroundColor: theme.palette.background.dark,
-  }
+    maxWidth: 1024
+  },
 }));
 
 const EnrollmentListView = () => {
   const classes = useStyles();
+  const [loading] = useState(false);
+  const [enrollments] = useState(data);
 
   return (
     <Page
-      className={classes.root}
-      title=""
+      title="Matrícula"
     >
       <Container maxWidth={false}>
-
         <Box mt={3}>
-          <h1>Matrícula</h1>
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+          >
+            <Grid item xs container direction="column" className={classes.root}>
+              <Component enrollments={enrollments} loading={loading} />
+            </Grid>
+          </Grid>
         </Box>
       </Container>
     </Page>

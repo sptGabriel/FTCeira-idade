6e625eb789a23d94c1aa9a0e8e03bb9a6b25006f
Jobson de Almeida <jobson.ra@outlook.com>
2021-05-20 22:43:57 -0300

    refactor: removing filters and using simple bars in the graphs

--- packages/frontend/src/views/dashboard/DashboardView/data1.js
+++ packages/frontend/src/views/dashboard/DashboardView/data1.js
@@ -3,11 +3,11 @@ import { colors } from '@material-ui/core';
 export default
 {
   datasets: [
-    {
-      label: 'Total',
-      backgroundColor: '#0093ff',
-      data: [18, 5, 19, 27]
-    },
+    // {
+    //   label: 'Total',
+    //   backgroundColor: '#0093ff',
+    //   data: [18, 5, 19, 27]
+    // },
     {
       label: 'Média',
       backgroundColor: colors.grey[200],

1e197c4be4092318ecb45e1d3d5fabd201ba015e
Jobson de Almeida <jobson.ra@outlook.com>
2021-05-20 23:12:08 -0300

    style: header size

--- packages/frontend/src/views/teacher/TeacherEdit/TeacherEditDetails.js
+++ packages/frontend/src/views/teacher/TeacherEdit/TeacherEditDetails.js
@@ -6,32 +6,14 @@ import {
   Button,
   Container,
   TextField,
-  makeStyles,
   Grid,
   Card,
   CardHeader,
   Divider,
   CardContent
 } from '@material-ui/core';
-import Page from 'src/components/Page';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    backgroundColor: theme.palette.background.dark,
-    height: '100%',
-    paddingBottom: theme.spacing(3),
-    paddingTop: theme.spacing(3)
-  },
-  texto: {
-    height: 44
-  }
-}));
 
 const TeacherEditDetails = () => {
-  const classes = useStyles();
-  // const navigate = useNavigate();
-
-  // dados do docente selecionado
   const [teacher] = useState({
     firstName: 'hsdfh',
     lastName: 'gjfgjhfgh',
@@ -41,32 +23,23 @@ const TeacherEditDetails = () => {
   });
 
   return (
-    <Page
-      className={classes.root}
-      title="Editar docente"
-    >
-      <Box
-        display="flex"
-        flexDirection="column"
-        height="100%"
-        justifyContent="center"
-      >
-        <Container maxWidth="sm">
-          <Card>
-            <CardHeader
-              subheader="As informações podem ser editadas"
-              title="Editar cadastro"
-            />
-            <Divider />
-            <Formik
-              initialValues={{
-                firstName: teacher.firstName,
-                lastName: teacher.lastName,
-                cpf: teacher.cpf,
-                email: teacher.email,
-                phone: teacher.phone
-              }}
-              validationSchema={
+    <Container maxWidth="sm">
+      <Card>
+        <CardHeader
+          title="Editar docente"
+          subheader="As informações podem ser editadas"
+          titleTypographyProps={{ variant: 'h4' }}
+        />
+        <Divider />
+        <Formik
+          initialValues={{
+            firstName: teacher.firstName,
+            lastName: teacher.lastName,
+            cpf: teacher.cpf,
+            email: teacher.email,
+            phone: teacher.phone
+          }}
+          validationSchema={
               Yup.object().shape({
                 firstName: Yup.string().max(255).required('Nome obrigatório'),
                 lastName: Yup.string().max(255).required('Sobrenome obrigatório'),
@@ -75,115 +48,114 @@ const TeacherEditDetails = () => {
                 phone: Yup.string().min(10, '99 99999-9999').max(15, 'Esse campo deve ter no máximo 15 caracteres')
               })
             }
-              onSubmit={(values) => {
-                // navigate('/app/teachers', { replace: false });
-                console.log(JSON.stringify(values, null, 2));
-              }}
-            >
-              {({
-                errors,
-                handleBlur,
-                handleChange,
-                handleSubmit,
-                isSubmitting,
-                touched,
-                values
-              }) => (
-                <form onSubmit={handleSubmit}>
-                  <CardContent>
+          onSubmit={(values) => {
+            // navigate('/app/teachers', { replace: false });
+            console.log(JSON.stringify(values, null, 2));
+          }}
+        >
+          {({
+            errors,
+            handleBlur,
+            handleChange,
+            handleSubmit,
+            isSubmitting,
+            touched,
+            values
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <CardContent>
+                <TextField
+                  error={Boolean(touched.firstName && errors.firstName)}
+                  fullWidth
+                  required
+                  helperText={touched.firstName && errors.firstName}
+                  label="Nome"
+                  margin="normal"
+                  name="firstName"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.firstName}
+                  variant="outlined"
+                />
+                <TextField
+                  error={Boolean(touched.lastName && errors.lastName)}
+                  fullWidth
+                  required
+                  helperText={touched.lastName && errors.lastName}
+                  label="Sobrenome"
+                  margin="normal"
+                  name="lastName"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.lastName}
+                  variant="outlined"
+                />
+                <TextField
+                  error={Boolean(touched.email && errors.email)}
+                  fullWidth
+                  required
+                  helperText={touched.email && errors.email}
+                  label="Email"
+                  margin="normal"
+                  name="email"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  type="email"
+                  value={values.email}
+                  variant="outlined"
+                />
+                <Grid container spacing={3}>
+                  <Grid item xs>
                     <TextField
-                      error={Boolean(touched.firstName && errors.firstName)}
+                      error={Boolean(touched.cpf && errors.cpf)}
                       fullWidth
                       required
-                      helperText={touched.firstName && errors.firstName}
-                      label="Nome"
+                      disabled
+                      helperText={touched.cpf && errors.cpf}
+                      label="CPF"
                       margin="normal"
-                      name="firstName"
+                      name="cpf"
                       onBlur={handleBlur}
                       onChange={handleChange}
-                      value={values.firstName}
+                      value={values.cpf}
                       variant="outlined"
                     />
+                  </Grid>
+                  <Grid item xs>
                     <TextField
-                      error={Boolean(touched.lastName && errors.lastName)}
+                      error={Boolean(touched.phone && errors.phone)}
                       fullWidth
-                      required
-                      helperText={touched.lastName && errors.lastName}
-                      label="Sobrenome"
+                      helperText={touched.phone && errors.phone}
+                      label="Telefone"
                       margin="normal"
-                      name="lastName"
+                      name="phone"
                       onBlur={handleBlur}
                       onChange={handleChange}
-                      value={values.lastName}
+                      type="phone"
+                      value={values.phone}
                       variant="outlined"
                     />
-                    <TextField
-                      error={Boolean(touched.email && errors.email)}
-                      fullWidth
-                      required
-                      helperText={touched.email && errors.email}
-                      label="Email"
-                      margin="normal"
-                      name="email"
-                      onBlur={handleBlur}
-                      onChange={handleChange}
-                      type="email"
-                      value={values.email}
-                      variant="outlined"
-                    />
-                    <Grid container spacing={3}>
-                      <Grid item xs>
-                        <TextField
-                          error={Boolean(touched.cpf && errors.cpf)}
-                          fullWidth
-                          required
-                          helperText={touched.cpf && errors.cpf}
-                          label="CPF"
-                          margin="normal"
-                          name="cpf"
-                          onBlur={handleBlur}
-                          onChange={handleChange}
-                          value={values.cpf}
-                          variant="outlined"
-                        />
-                      </Grid>
-                      <Grid item xs>
-                        <TextField
-                          error={Boolean(touched.phone && errors.phone)}
-                          fullWidth
-                          helperText={touched.phone && errors.phone}
-                          label="Telefone"
-                          margin="normal"
-                          name="phone"
-                          onBlur={handleBlur}
-                          onChange={handleChange}
-                          type="phone"
-                          value={values.phone}
-                          variant="outlined"
-                        />
-                      </Grid>
-                    </Grid>
-                  </CardContent>
-                  <Divider />
-                  <Box my={2}>
-                    <Button
-                      color="primary"
-                      disabled={isSubmitting}
-                      fullWidth
-                      size="large"
-                      type="submit"
-                      variant="contained"
-                    >
-                      Salvar
-                    </Button>
-                  </Box>
-                </form>
-              )}
-            </Formik>
-          </Card>
-        </Container>
-      </Box>
-    </Page>
+                  </Grid>
+                </Grid>
+              </CardContent>
+              <Divider />
+              <Box my={2}>
+                <Button
+                  color="primary"
+                  disabled={isSubmitting}
+                  fullWidth
+                  size="large"
+                  type="submit"
+                  variant="contained"
+                >
+                  Salvar
+                </Button>
+              </Box>
+            </form>
+          )}
+        </Formik>
+      </Card>
+    </Container>
   );
 };
 

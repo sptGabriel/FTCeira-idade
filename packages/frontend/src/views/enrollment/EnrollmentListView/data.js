47b5a627ac83e7ecc6d36059c1b9288bee80fb00
Jobson de Almeida <jobson.ra@outlook.com>
2021-05-21 19:26:41 -0300

    refactor: initial enrollment list code

---
+++ packages/frontend/src/views/enrollment/EnrollmentListView/data.js
@@ -0,0 +1,25 @@
+import { v4 as uuid } from 'uuid';
+
+export default [
+  {
+    id: uuid(),
+    admission: '20/04/2021',
+    course: 'Curso de redes sociais II',
+    classroom: 'CRS00122',
+    status: 'pendente'
+  },
+  {
+    id: uuid(),
+    admission: '01/04/2021',
+    course: 'Curso de redes sociais I',
+    classroom: 'CRS00121',
+    status: 'ativa'
+  },
+  {
+    id: uuid(),
+    admission: '01/01/2021',
+    course: 'Curso de redes sociais I',
+    classroom: 'CRS00121',
+    status: 'concluído'
+  }
+];

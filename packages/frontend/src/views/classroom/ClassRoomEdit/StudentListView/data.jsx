2803f39f557e93517e61d0db8b84b0d89fb03273
Jobson de Almeida <jobson.ra@outlook.com>
2021-05-21 09:19:46 -0300

    refactor: tab navigation, class-related features

---
+++ packages/frontend/src/views/classroom/ClassRoomEdit/StudentListView/data.jsx
@@ -0,0 +1,114 @@
+import { v4 as uuid } from 'uuid';
+
+export default [
+  {
+    id: uuid(),
+    avatarUrl: '/static/images/avatars/avatar.png',
+    email: 'aaa.bbb@fterceiridade',
+    firstname: 'Aaa Bbb',
+    lastname: 'Absf',
+    birth: '01/02/2000',
+    phone: '73 93044-3097',
+    cpf: '123.456.798-00',
+    status: 'ativo'
+  },
+  {
+    id: uuid(),
+    avatarUrl: '/static/images/avatars/avatar.png',
+    email: 'aaa.bbb@fterceiridade',
+    firstname: 'Ddd Bbb',
+    lastname: 'Absf',
+    birth: '01/02/2000',
+    phone: '73 93044-3097',
+    cpf: '123.456.798-00',
+    status: 'ativo'
+  },
+  {
+    id: uuid(),
+    avatarUrl: '',
+    email: 'aaa.bbb@fterceiridade',
+    firstname: 'Eaa Abb',
+    lastname: 'Absf',
+    birth: '01/02/2000',
+    phone: '73 93044-3097',
+    cpf: '123.456.798-00',
+    status: 'ativo'
+  },
+  {
+    id: uuid(),
+    avatarUrl: '',
+    email: 'aaa.bbb@fterceiridade',
+    firstname: 'Faa Cbb',
+    lastname: 'Absf',
+    birth: '01/02/2000',
+    phone: '73 93044-3097',
+    cpf: '123.456.798-00',
+    status: 'ativo'
+  },
+  {
+    id: uuid(),
+    avatarUrl: '/static/images/avatars/avatar.png',
+    email: 'aaa.bbb@fterceiridade',
+    firstname: 'Aaa Bbb',
+    lastname: 'Absf',
+    birth: '01/02/2000',
+    phone: '73 93044-3097',
+    cpf: '123.456.798-00',
+    status: 'ativo'
+  },
+  {
+    id: uuid(),
+    avatarUrl: '',
+    email: 'aaa.bbb@fterceiridade',
+    firstname: 'Haa Abb',
+    lastname: 'Absf',
+    birth: '01/02/2000',
+    phone: '73 93044-3097',
+    cpf: '123.456.798-00',
+    status: 'ativo'
+  },
+  {
+    id: uuid(),
+    avatarUrl: '/static/images/avatars/avatar.png',
+    email: 'aaa.bbb@fterceiridade',
+    firstname: 'Haa Ebb',
+    lastname: 'Absf',
+    birth: '01/02/2000',
+    phone: '73 93044-3097',
+    cpf: '123.456.798-00',
+    status: 'ativo'
+  },
+  {
+    id: uuid(),
+    avatarUrl: '/static/images/avatars/avatar.png',
+    email: 'aaa.bbb@fterceiridade',
+    firstname: 'Saa Jbb',
+    lastname: 'Absf',
+    birth: '01/02/2000',
+    phone: '73 93044-3097',
+    cpf: '123.456.798-00',
+    status: 'ativo'
+  },
+  {
+    id: uuid(),
+    avatarUrl: '',
+    email: 'aaa.bbb@fterceiridade',
+    firstname: 'Taa Jbb',
+    lastname: 'Absf',
+    birth: '01/02/2000',
+    phone: '73 93044-3097',
+    cpf: '123.456.798-00',
+    status: 'ativo'
+  },
+  {
+    id: uuid(),
+    avatarUrl: '',
+    email: 'aaa.bbb@fterceiridade',
+    firstname: 'Eu Me',
+    lastname: 'Absf',
+    birth: '01/02/2000',
+    phone: '73 93044-3097',
+    cpf: '123.456.798-00',
+    status: 'ativo'
+  }
+];

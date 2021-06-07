1aa954564903355d7fc0aa039e649ff8e1485f3c
Jobson de Almeida <jobson.ra@outlook.com>
2021-06-07 01:46:15 -0300

    feat: student registration

--- packages/frontend/src/views/course/CourseView/CourseViewDetails.jsx
+++ packages/frontend/src/views/course/CourseView/CourseViewDetails.jsx
@@ -1,45 +1,66 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Button,
+  CardMedia,
   CardActionArea,
   Grid,
-  Container,
-  CardMedia,
+  Radio,
+  FormControlLabel,
+  RadioGroup,
   Typography
 } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { useForm, Controller } from 'react-hook-form';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+  },
+  info: {
+    marginTop: theme.spacing(5),
+  },
+  button: {
+    marginTop: theme.spacing(10),
+    height: 66,
+    fontSize: 20,
+  }
+}));
 
 const CourseViewDetails = () => {
-  // dados do curso selecionado ------------------
-  const getCourse = localStorage.getItem('course_home');
+  const classes = useStyles();
+  const getUser = JSON.parse(localStorage.getItem('userData'));
+  const [user] = useState(getUser.role);
+  const getCourse = JSON.parse(localStorage.getItem('course_home'));
   const [course, setCourse] = useState({
     id: '',
     description: '',
     media: '',
-    tittle: '',
-    shift: '',
-    students: '',
+    tittle: ''
   });
-
   useEffect(() => {
+    console.log(user);
     setCourse({
       id: getCourse ? getCourse.id : '',
       description: getCourse ? getCourse.description : '',
-      media: getCourse ? getCourse.media : '',
+      media: '/static/images/courses/si.jpg', // getCourse ? getCourse.media : '',
       tittle: getCourse ? getCourse.tittle : '',
-      shift: 'falta essa variavel',
-      students: 'falta essa variavel'
     });
   }, []);
 
-  // const [avatar] = useState(course.avatar);
-  //--------------------
+  const {
+    handleSubmit, control, setValue, watch
+  } = useForm({
+    defaultValues: {
+      courseId: getCourse ? getCourse.id : '',
+      shift: ''
+    }
+  });
 
-  const handleSubmit = useCallback(() => {
-    console.log(course.tittle, course.description, course.students, course.shift);
-  }, [course]);
+  const onSubmit = (data) => {
+    console.log(JSON.stringify(data, null, 2));
+  };
 
   return (
-    <Container>
+    <div>
       <Grid container spacing={3}>
 
         <Grid item xs={8}>
@@ -52,40 +73,89 @@ const CourseViewDetails = () => {
           </CardActionArea>
         </Grid>
 
-        <Grid item xs={4}>
-          <Typography variant="h1" component="h2">
-            {course.tittle}
-          </Typography>
-          <br />
-          <Typography variant="body1" component="p">
-            {course.description}
-          </Typography>
-          <br />
-          <Typography variant="body1" component="h1">
-            {course.students}
-            {' '}
-            vagas
-          </Typography>
-          <br />
-          <Typography variant="h2" component="h1">
-            {course.shift}
-          </Typography>
-          <br />
-          <Typography variant="body1" component="h1">
-            ::: ALTERAR ESSA PÁGINA :::
-          </Typography>
-          <br />
-          <Button
-            color="primary"
-            variant="outlined"
-            onClick={handleSubmit}
+        <Grid
+          item
+        >
+          <Grid
+            container
+            spacing={10}
+            sx={2}
+            direction="column"
+            justify="center"
+            alignItems="center"
           >
-            INSCREVA-SE
-          </Button>
+            <Grid item className={classes.info}>
+              <Typography variant="h1" component="h2">
+                {course.tittle}
+              </Typography>
+              <br />
+              <Typography variant="h3" component="h1">
+                Descrição:
+                {' '}
+                {course.description}
+              </Typography>
+            </Grid>
+            <Grid item>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <Typography variant="h2" component="h1">
+                  Defina o turno desejado
+                </Typography>
+                <br />
+                <Controller
+                  name="shift"
+                  control={control}
+                  render={({ field }) => (
+                    <RadioGroup
+                      row
+                      aria-label="shift"
+                      {...field}
+                    >
+                      <FormControlLabel
+                        value="matunino"
+                        control={(
+                          <Radio
+                            onChange={
+                            (item) => { setValue('shift', item.target.value); }
+                          }
+                            required
+                          />
+                    )}
+                        label={<Typography variant="h3" component="h1">Matutino</Typography>}
+                      />
+                      <FormControlLabel
+                        value="vespertino"
+                        control={(
+                          <Radio
+                            required
+                            onChange={
+                              (item) => { setValue('shift', item.target.value); }
+                            }
+                          />
+                      )}
+                        label={<Typography variant="h3" component="h1">Vespertino</Typography>}
+                      />
+                    </RadioGroup>
+                  )}
+                />
+                <Grid item>
+                  <Button
+                    disabled={user !== 'student'}
+                    color="primary"
+                    variant="contained"
+                    size="large"
+                    type="submit"
+                    fullWidth
+                    className={classes.button}
+                  >
+                    INSCREVA-SE
+                  </Button>
+                </Grid>
+              </form>
+            </Grid>
+          </Grid>
         </Grid>
-
       </Grid>
-    </Container>
+    </div>
   );
 };
 
--- packages/frontend/src/views/course/CourseView/index.jsx
+++ packages/frontend/src/views/course/CourseView/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  Container,
+  // Container,
   makeStyles
 } from '@material-ui/core';
 import Page from 'src/components/Page';
@@ -10,8 +10,10 @@ const useStyles = makeStyles((theme) => ({
   root: {
     backgroundColor: theme.palette.background.dark,
     minHeight: '100%',
+    paddingLeft: theme.spacing(3),
     paddingBottom: theme.spacing(3),
-    paddingTop: theme.spacing(3)
+    paddingTop: theme.spacing(3),
+    flexGrow: 1,
   }
 }));
 
@@ -23,12 +25,14 @@ const CourseEdit = () => {
       className={classes.root}
       title="Curso"
     >
-      <Container maxWidth="lg">
-        {/* <Container maxWidth="false"> */}
-        <div>
-          <CourseViewDetails />
-        </div>
-      </Container>
+      <div>
+        {/* <Container maxWidth="lg"> */}
+        {/* <Container maxWidth="lg"> */}
+
+        <CourseViewDetails />
+
+        {/* </Container> */}
+      </div>
     </Page>
   );
 };

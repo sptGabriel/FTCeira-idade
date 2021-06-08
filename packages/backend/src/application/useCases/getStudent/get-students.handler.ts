a6da8c1cde0d170c4705f3b4daf745b8db6c586f
Gabriel Costa Cerqueira <gabrielcerqueira96@hotmail.com>
2021-06-07 23:49:57 -0300

    refactor: get students & perosn returns

--- packages/backend/src/application/useCases/getStudent/get-students.handler.ts
+++ packages/backend/src/application/useCases/getStudent/get-students.handler.ts
@@ -1,19 +1,22 @@
-import { inject, injectable } from 'tsyringe'
+import { inject,injectable } from 'tsyringe'
 import { TransactionalRepository } from '~/shared/core/uow/transactional-repo'
-import Person from '~/modules/person/domain/person.entity'
+import User from '~/modules/person/domain/user.entity'
 
 @injectable()
 export class GetAllStudents {
   constructor(
     @inject(TransactionalRepository)
     private transactionalRepo: TransactionalRepository,
-  ) {}
+  ) { }
 
-  get personRepository() {
-    return this.transactionalRepo.getRepository(Person)
+  get userRepository() {
+    return this.transactionalRepo.getRepository(User)
   }
 
   async execute(): Promise<any> {
-    return await this.personRepository.find({ where: { role: 'student' } })
+    const students: User[] = await this.userRepository.find({
+      where: { person:{role: 'student' }}
+    })
+    return students.map((st) => st.toJson())
   }
 }
--- packages/backend/src/application/useCases/getTeacher/get-teachers.handler.ts
+++ packages/backend/src/application/useCases/getTeacher/get-teachers.handler.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from 'tsyringe'
 import { TransactionalRepository } from '~/shared/core/uow/transactional-repo'
 import Person from '~/modules/person/domain/person.entity'
+import User from '~/modules/person/domain/user.entity'
 
 @injectable()
 export class GetAllTeachers {
@@ -9,11 +10,14 @@ export class GetAllTeachers {
     private transactionalRepo: TransactionalRepository,
   ) {}
 
-  get personRepository() {
-    return this.transactionalRepo.getRepository(Person)
+  get userRepository() {
+    return this.transactionalRepo.getRepository(User)
   }
 
   async execute(): Promise<any> {
-    return await this.personRepository.find({ where: { role: 'teacher' } })
+    const teachers: User[] = await this.userRepository.find({
+      where: { person:{role: 'student' }}
+    })
+    return teachers.map((teachers) => teachers.toJson())
   }
 }
